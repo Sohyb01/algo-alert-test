93e1a67eaaf9ee4da03da55fe913f80a6d6cd923
Sohyb01 <sohyb0155@gmail.com>
2023-12-25 00:30:45 +0200

    Changed manually insterted stripe publisha key

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,9 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 let stripePromise: Promise<Stripe | null>;
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(
-      "pk_test_51NmOWMCJ7W2t3weqPgDQAiQBIEuZp4rNAphapt05icaCLoQu0cb087MCUIBGwzkBsHwDmNMT5RA6o0OREAkGCxjX00CNSy6UIM"
-    );
+    stripePromise = loadStripe(`${process.env.STRIPE_PUBLISHABLE_KEY}`);
   }
   return stripePromise;
 };
