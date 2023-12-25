f6cdf8daebd830ea26a76f876e2475a631601293
Sohyb01 <sohyb0155@gmail.com>
2023-12-25 03:59:53 +0200

    remove date picker if loading

--- app/components/DataTableContainer.tsx
+++ app/components/DataTableContainer.tsx
@@ -32,29 +32,31 @@ const DataTableContainer = (props: any) => {
   return (
     <div className="flex flex-col items-start">
       {/* Date Input and label */}
-      <div className="flex gap-2 items-center text-start text-white text-base">
-        <p>Current date:</p>
-        <select
-          className="scroll-styling bg-slate-950 p-1"
-          onChange={handleSelectChange}
-        >
-          <option
-            className="bg-slate-900 hover:bg-slate-700"
-            defaultValue={getOptionsMarketStatus()}
+      {!loading && (
+        <div className="flex gap-2 items-center text-start text-white text-base">
+          <p>Current date:</p>
+          <select
+            className="scroll-styling bg-slate-950 p-1"
+            onChange={handleSelectChange}
           >
-            {getOptionsMarketStatus()}
-          </option>
-          {datesArray.map((date, index) => (
             <option
               className="bg-slate-900 hover:bg-slate-700"
-              key={index}
-              value={date}
+              defaultValue={getOptionsMarketStatus()}
             >
-              {date}
+              {getOptionsMarketStatus()}
             </option>
-          ))}
-        </select>
-      </div>
+            {datesArray.map((date, index) => (
+              <option
+                className="bg-slate-900 hover:bg-slate-700"
+                key={index}
+                value={date}
+              >
+                {date}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
       {loading || dayData.length === 0 ? (
         <LoadingSmall />
       ) : (
