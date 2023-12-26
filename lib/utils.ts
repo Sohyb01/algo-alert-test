138c9eb24ac2977b1e7b025234d8a3c4f20207fb
Sohyb01 <sohyb0155@gmail.com>
2023-12-26 22:37:14 +0200

    removed vercel.json

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,7 +14,10 @@ const getStripe = () => {
     );
     // rexxors publishable key manually
     stripePromise = loadStripe(
-      `pk_test_51OQf4QJYh1nryesbYtIE26ckuRMdJXMM335T0Z6ObBHJTcFdS4U0ssybMtcb3gvvVohYJ1ooUpe8b0kI8q237HKx00bIJF6W0m`
+      // Rexxor's test key:
+      // `pk_test_51OQf4QJYh1nryesbYtIE26ckuRMdJXMM335T0Z6ObBHJTcFdS4U0ssybMtcb3gvvVohYJ1ooUpe8b0kI8q237HKx00bIJF6W0m`
+      // Sohyb's Testing key:
+      `pk_test_51NmOWMCJ7W2t3weqPgDQAiQBIEuZp4rNAphapt05icaCLoQu0cb087MCUIBGwzkBsHwDmNMT5RA6o0OREAkGCxjX00CNSy6UIM`
     );
   }
   return stripePromise;
