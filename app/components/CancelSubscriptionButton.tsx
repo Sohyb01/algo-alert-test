e7dc1f34a0e5aa76600c2a08a7e81b323b873109
Sohyb01 <sohyb0155@gmail.com>
2023-12-26 22:05:01 +0200

    added dialogs to cancel and upgrade buttons

--- app/components/CancelSubscriptionButton.tsx
+++ app/components/CancelSubscriptionButton.tsx
@@ -5,6 +5,19 @@ import { toast } from "react-toastify";
 import React from "react";
 import { revalidatePathManually } from "../actions/revalidatePath";
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
 const CancelSubscriptionButton = () => {
   const cancelSubscription = async () => {
     try {
@@ -21,12 +34,30 @@ const CancelSubscriptionButton = () => {
   };
 
   return (
-    <button
-      onClick={cancelSubscription}
-      className="text-white px-4 rounded-full py-2 border-red-400 border-[1px] border-solid"
-    >
-      Cancel Subscription
-    </button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Cancel Subscription</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you sure you want to cancel your subscription?
+          </AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={cancelSubscription}>
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+    // <button
+    //   onClick={cancelSubscription}
+    //   className="text-white px-4 rounded-full py-2 border-red-400 border-[1px] border-solid"
+    // >
+    //   Cancel Subscription
+    // </button>
   );
 };
 
--- app/components/UpgradeToYearlyButton.tsx
+++ app/components/UpgradeToYearlyButton.tsx
@@ -1,8 +1,21 @@
 "use client";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
 import React from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { revalidatePathManually } from "../actions/revalidatePath";
+import { redirectManually } from "../actions/redirectAction";
 
 const UpgradeToYearlyButton = () => {
   const upgradeSubscription = async () => {
@@ -11,34 +24,43 @@ const UpgradeToYearlyButton = () => {
       const { subscription } = await res.json();
       if (subscription.status === 200) {
         // revalidatePathManually("/");
-        toast("Subscription upgraded successfully!");
+        toast.success("Subscription upgraded successfully!");
+        redirectManually("/");
       }
     } catch (error) {
       // toast("An error as occurred, please try again or contact a moderator.");
       // console.log(error);
       toast.error(
-        "An error as occurred, please try again or contact a moderator.",
-        {
-          position: "bottom-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-        }
+        "An error as occurred, please try again or contact a moderator."
       );
     }
   };
 
   return (
-    <button
-      onClick={upgradeSubscription}
-      className="text-white px-4 rounded-full py-2 border-teal-400 border-[1px] border-solid"
-    >
-      Upgrade to Yearly Subscription
-    </button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Upgrade to Yearly Subscription</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you sure you want to upgrade your subscription?
+          </AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={upgradeSubscription}>
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+    // <button
+    //   onClick={upgradeSubscription}
+    //   className="text-white px-4 rounded-full py-2 border-teal-400 border-[1px] border-solid"
+    // >
+    //   Upgrade to Yearly Subscription
+    // </button>
   );
 };
 
